6fdaa777de03526c148f19245ddf55f8e57297cb
Pablo <pablo.cicare@gmail.com>
2020-06-16 01:38:32 -0300

    mobile upgrade

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const btnToggle = document.querySelector('.toggle-btn');
 const btnActive = document.querySelector('.li-btn');
+const menuMobile = document.querySelector('.menu-mobile')
 
 btnToggle.addEventListener('click', function() {
     document.querySelector('.menu-mobile').classList.toggle('active');
     document.querySelector('.toggle-btn').classList.toggle('active');
-    console.log("clicked");
 });
