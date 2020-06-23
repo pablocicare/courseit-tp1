35ef94601f9cba4b1be4ae559beebca0065af2cd
Pablo <pablo.cicare@gmail.com>
2020-06-23 17:51:33 -0300

    carousel fixes

--- js/index.js
+++ js/index.js
@@ -9,3 +9,44 @@ btnActive.addEventListener('click', function() {
     document.querySelector('.menu-mobile').classList.remove('active');
     console.log("click")
 });
+
+$(document).ready(function(){
+
+    $('.carousel-series').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        mobileFirst: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+
+        // responsive: [
+        //   {
+        //     breakpoint: 1024,
+        //     settings: {
+        //       slidesToShow: 4,
+        //       slidesToScroll: 1,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 1
+        //     }
+        //   },
+        //   // You can unslick at a given breakpoint now by adding:
+        //   // settings: "unslick"
+        //   // instead of a settings object
+        // ]
+      });
+
+  });
+
+  
