d3b3ac159e09a77a4917285cd16ae4c65f728e2e
Pablo <pablo.cicare@gmail.com>
2020-06-22 21:26:15 -0300

    desktop view

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 const btnToggle = document.querySelector('.toggle-btn');
 const btnActive = document.querySelector('.li-btn');
-const menuMobile = document.querySelector('.menu-mobile')
 
 btnToggle.addEventListener('click', function() {
     document.querySelector('.menu-mobile').classList.toggle('active');
-    document.querySelector('.toggle-btn').classList.toggle('active');
+});
+
+btnActive.addEventListener('click', function() {
+    document.querySelector('.menu-mobile').classList.remove('active');
+    console.log("click")
 });
